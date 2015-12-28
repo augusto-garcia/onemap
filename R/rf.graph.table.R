8ad1a2e96fbb7820ad31227ae5c85151f877c745
Marcelo <mmollina@gmail.com>
2015-12-28 01:52:51 +0000

    Update: rf.graph.table

--- R/rf.graph.table.R
+++ R/rf.graph.table.R
@@ -114,8 +114,8 @@ rf.graph.table <- function(input.seq, scale=1, axis.cex=1, main=NULL, inter=TRUE
                           return(m)
                       }, input.seq$seq.num
                       )
-        mat<-t(get_mat_rf_out(input.seq, LOD=TRUE))
-        mat.rf<-t(get_mat_rf_out(input.seq, LOD=FALSE))
+        mat<-t(get_mat_rf_out(input.seq, LOD=TRUE,  max.rf = 0.501, min.LOD = -0.1))
+        mat.rf<-t(get_mat_rf_out(input.seq, LOD=FALSE,  max.rf = 0.501, min.LOD = -0.1))
     }
     else
     {
@@ -131,8 +131,8 @@ rf.graph.table <- function(input.seq, scale=1, axis.cex=1, main=NULL, inter=TRUE
                 LOD[j,i]<-LOD[i,j]<-get(input.seq$twopt)$analysis[z[1], z[2]]
             }
         }
-        mat<-t(get_mat_rf_in(input.seq, LOD=TRUE))
-        mat.rf<-t(get_mat_rf_in(input.seq, LOD=FALSE))
+        mat<-t(get_mat_rf_in(input.seq, LOD=TRUE,  max.rf = 0.501, min.LOD = -0.1))
+        mat.rf<-t(get_mat_rf_in(input.seq, LOD=FALSE,  max.rf = 0.501, min.LOD = -0.1))
     }
     
     ##Scaling the LODs to print them properly 
@@ -146,12 +146,14 @@ rf.graph.table <- function(input.seq, scale=1, axis.cex=1, main=NULL, inter=TRUE
     mat[row(mat) < col(mat)] <- 0.5 - log.LOD/max.scale
     diag(mat)<-NA
     range.scaled.LOD<-range(as.dist(t(mat)), na.rm=TRUE)
+    
     ##Write multipoint estimates
     for (i in 1:(n.mrk-1)){
         mat[i+1,i] <- input.seq$seq.rf[i]
         mat.rf[i+1,i] <- mat.rf[i,i+1] <- input.seq$seq.rf[i]
     }
     colnames(mat) <- colnames(get(input.seq$data.name, pos=1)$geno)[input.seq$seq.num]
+    
     ##Write NAs in two-point recombination fractions between markers of type D1 and D2
     types <- get(input.seq$data.name, pos=1)$segr.type[input.seq$seq.num]
     which.D1D2<-outer((substr(types, 1,2)=="D1"),(substr(types, 1,2)=="D2"))
@@ -162,6 +164,7 @@ rf.graph.table <- function(input.seq, scale=1, axis.cex=1, main=NULL, inter=TRUE
     for(i in 1:(ncol(which.D1D2)-1)) which.D1D2[diag.si[1,i],diag.si[2,i]] <- which.D1D2[diag.si[2,i],diag.si[1,i]] <- 1
     mat<-mat*which.D1D2
     missing<-100*apply(get(input.seq$data.name, pos=1)$geno[,input.seq$seq.num],2, function(x) sum(x==0))/get(input.seq$data.name, pos=1)$n.ind
+    
     ##info.graph contains all information necessary to plot the graphics 
     info.graph <- list(mat=mat,
                        mat.rf=mat.rf,
@@ -183,6 +186,7 @@ rf.graph.table <- function(input.seq, scale=1, axis.cex=1, main=NULL, inter=TRUE
         plotFunction.out(info.graph=info.graph, cex=axis.cex, main=main)
     else draw.rf.inter(info.graph=info.graph,scale=scale,cex=axis.cex)
 }
+
 ##This function plots the recombination fraction WITHOUT using interactive Tcl-Tk interface
 plotFunction.out <- function(info.graph, cex, main){
     if(!info.graph$colorkey){
@@ -199,7 +203,7 @@ plotFunction.out <- function(info.graph, cex, main){
     y.adj<- .8/(-2+info.graph$n*2)
     if(is.null(main)) main="LOD (above diag.) and Recombination Fraction Matrix"
     image(info.graph$mat, axes=FALSE,
-          col=rev(tim.colors(200)),#rainbow(n=500, start=min(info.graph$mat, na.rm=TRUE)*1.3, end=max(info.graph$mat, na.rm=TRUE)*1.3),
+          col=rev(tim.colors(200)),
           useRaster=TRUE)
     if(!info.graph$colorkey)
         title(main = main, cex.main=cex)
@@ -218,8 +222,9 @@ plotFunction.out <- function(info.graph, cex, main){
     if(info.graph$colorkey){
         par(cex.axis=cex, las=1)
         plot(x=rep(1,200), y=seq(from=info.graph$range.rf[1], to=info.graph$range.rf[2], length.out=200), xlim=c(1,1.5), cex=2,
-             col=rev(tim.colors(200)),#rainbow(n=200, start=info.graph$range.rf[1]*1.3, end=info.graph$range.rf[2]*1.3),
+             col=rev(tim.colors(200)),
              pch=15, axes=FALSE, xlab="", ylab="")
+        mtext("rec. frac.",1, adj = 0)
         Axis(x=info.graph$range.rf,
              at=round(seq(from=info.graph$range.rf[1], to=info.graph$range.rf[2], length.out=10),2),
              side=4,
@@ -227,14 +232,13 @@ plotFunction.out <- function(info.graph, cex, main){
         plot(0,type="n",axes=FALSE, xlab="", ylab="")#to fill the upper right corner
         plot(x=seq(from=info.graph$range.LOD[1], to=info.graph$range.LOD[2], length.out=200), y=rep(1,200),
              ylim=c(1,1.5),
-             ##xlim=c(0,info.graph$range.LOD[2]+5),
              cex=2,
-             col=tim.colors(200),#rev(rainbow(n=200, start=info.graph$range.scaled.LOD[1]*1.3, end=info.graph$range.scaled.LOD[2]*1.3)),
+             col=tim.colors(200),
              pch=15,
              axes=FALSE,
              xlab="",
              ylab="")
-        mtext("bla")
+        mtext("LOD",2, padj = 4.2)
         tk.pos<-seq(from=info.graph$range.scaled.LOD[1], to=info.graph$range.scaled.LOD[2], length.out=10)
         tk.lab<-rev(round(exp((.5-tk.pos)*info.graph$max.scale-info.graph$min.scale),1))
         Axis(x=info.graph$range.LOD,
@@ -246,9 +250,9 @@ plotFunction.out <- function(info.graph, cex, main){
     }
     par(params)
 }
+
 ##This function plots the recombination fraction using interactive Tcl-Tk interface
 draw.rf.inter<-function(info.graph, scale, cex){
-    ##Identical above, but used to Tcl-Tk plot
     plotFunction <- function()
     {
         if(info.graph$colorkey){
@@ -280,6 +284,7 @@ draw.rf.inter<-function(info.graph, scale, cex){
             plot(x=rep(1,200), y=seq(from=info.graph$range.rf[1], to=info.graph$range.rf[2], length.out=200), xlim=c(1,1.5), cex=2,
                  col=rev(tim.colors(200)),#rainbow(n=200, start=info.graph$range.rf[1]*1.3, end=info.graph$range.rf[2]*1.3),
                  pch=15, axes=FALSE, xlab="", ylab="")
+            mtext("rec. frac.",1, adj = 0)
             Axis(x=info.graph$range.rf,
                  at=round(seq(from=info.graph$range.rf[1], to=info.graph$range.rf[2], length.out=10),2),
                  side=4,
@@ -294,6 +299,7 @@ draw.rf.inter<-function(info.graph, scale, cex){
                  axes=FALSE,
                  xlab="",
                  ylab="")
+            mtext("LOD", 2, padj = 4.2)
             tk.pos<-seq(from=info.graph$range.scaled.LOD[1], to=info.graph$range.scaled.LOD[2], length.out=10)
             tk.lab<-rev(round(exp((.5-tk.pos)*info.graph$max.scale-info.graph$min.scale),1))
             Axis(x=info.graph$range.LOD,
@@ -301,7 +307,7 @@ draw.rf.inter<-function(info.graph, scale, cex){
                  labels=tk.lab,
                  side=3,
                  pos=c(1.1,0))
-            text(mean(info.graph$range.LOD), 1.5, "LOD (above diag.) and Recombination Fraction Matrix", cex=(4+scale)/4,pos=1)
+            text(mean(info.graph$range.LOD), 1.5, "LOD (above diag.) and Recombination Fraction Matrix", cex=round((4+scale)/4), vfont=c("sans serif", "bold italic"), pos=1)
         }
         par(params)
     }
@@ -344,14 +350,15 @@ draw.rf.inter<-function(info.graph, scale, cex){
                              "\n\n", format(info.graph$missing[x.n], digits=2), "% of missing data for this marker",
                              sep="")
             }
-            else if (class(get(info.graph$data.name, pos=1))=="f2.onemap" || class(get(info.graph$data.name, pos=1))=="bc.onemap"){
+            else if (class(get(info.graph$data.name, pos=1))=="f2.onemap" || class(get(info.graph$data.name, pos=1))=="bc.onemap")
+            {    
                 ##getting type of marker ('another classes')
                 if(info.graph$types[x.n]=="A.H.B")  mkt<-"AA : AB : BB (1:2:1) "
-        else if(info.graph$types[x.n]=="D.B")  mkt<-" Not BB : BB (3:1) "
-        else if(info.graph$types[x.n]=="C.A")  mkt<-" Not AA : AA (3:1) "
-        else if(info.graph$types[x.n]=="A.H")  mkt<-"AA : AB (1:1)"
-          else if(info.graph$types[x.n]=="M.X")  mkt<- "Mixed: Dominant & Co-dominant"
-        else stop ("invalid type of marker at marker ", info.graph$names[x.n])
+                else if(info.graph$types[x.n]=="D.B")  mkt<-" Not BB : BB (3:1) "
+                else if(info.graph$types[x.n]=="C.A")  mkt<-" Not AA : AA (3:1) "
+                else if(info.graph$types[x.n]=="A.H")  mkt<-"AA : AB (1:1)"
+                else if(info.graph$types[x.n]=="M.X")  mkt<- "Mixed: Dominant & Co-dominant"
+                else stop ("invalid type of marker at marker ", info.graph$names[x.n])
                 ##information for message box
                 msg <- paste("Marker name: \n    ", info.graph$names[x.n],
                              "\n\nMarker number:\n    ", mkx.n,
@@ -359,18 +366,18 @@ draw.rf.inter<-function(info.graph, scale, cex){
                              "\n\n", format(info.graph$missing[x.n], digits=2), "% of missing data for this marker",
                              sep="")
             }
-      else if (class(get(info.graph$data.name, pos=1))=="riself.onemap" || class(get(info.graph$data.name, pos=1))=="risib.onemap"){
-          ##getting type of marker ('another classes')
-          if(info.graph$types[x.n]=="A.B")  mkt<-"AA : BB (1:1)"
-        else stop ("invalid type of marker at marker ", info.graph$names[x.n])
-          ##information for message box
-          msg <- paste("Marker name: \n    ", info.graph$names[x.n],
-                       "\n\nMarker number:\n    ", mkx.n,
-                       "\n\nMarker type: \n    ", mkt,
-                       "\n\n", format(info.graph$missing[x.n], digits=2), "% of missing data for this marker",
-                       sep="")  
-      }
-      else stop("invalid type of cross")
+            else if (class(get(info.graph$data.name, pos=1))=="riself.onemap" || class(get(info.graph$data.name, pos=1))=="risib.onemap"){
+            ##getting type of marker ('another classes')
+                if(info.graph$types[x.n]=="A.B")  mkt<-"AA : BB (1:1)"
+                else stop ("invalid type of marker at marker ", info.graph$names[x.n])
+                ##information for message box
+                msg <- paste("Marker name: \n    ", info.graph$names[x.n],
+                             "\n\nMarker number:\n    ", mkx.n,
+                             "\n\nMarker type: \n    ", mkt,
+                             "\n\n", format(info.graph$missing[x.n], digits=2), "% of missing data for this marker",
+                             sep="")  
+            }
+            else stop("invalid type of cross")
             mbval<- tkmessageBox(title="Labeling Marker",message=msg,type="ok",icon="question")
         }
     else{
@@ -388,24 +395,22 @@ draw.rf.inter<-function(info.graph, scale, cex){
                              "\n    RR: ", round(info.graph$LOD$RR[x.n, y.n], digits=1),
                              sep="")
             }
-        else if (class(get(info.graph$data.name, pos=1))=="f2.onemap" || class(get(info.graph$data.name, pos=1))=="bc.onemap"){
+            else if (class(get(info.graph$data.name, pos=1))=="f2.onemap" || class(get(info.graph$data.name, pos=1))=="bc.onemap"){
             ##getting type of marker
             if(info.graph$types[x.n]=="A.H.B")  mktx<-"AA : AB : BB (1:2:1) "
-        else if(info.graph$types[x.n]=="D.B")  mktx<-" Not BB : BB (3:1) "
-        else if(info.graph$types[x.n]=="C.A")  mktx<-" Not AA : AA (3:1) "
-        else if(info.graph$types[x.n]=="A.H")  mktx<-"AA : AB (1:1)"
-          else if(info.graph$types[x.n]=="M.X")  mktx<- "Mixed: Dominant & Co-dominant"
-        else stop ("invalid type of marker at marker ", info.graph$names[x.n])
-            
-
+            else if(info.graph$types[x.n]=="D.B")  mktx<-" Not BB : BB (3:1) "
+            else if(info.graph$types[x.n]=="C.A")  mktx<-" Not AA : AA (3:1) "
+            else if(info.graph$types[x.n]=="A.H")  mktx<-"AA : AB (1:1)"
+            else if(info.graph$types[x.n]=="M.X")  mktx<- "Mixed: Dominant & Co-dominant"
+            else stop ("invalid type of marker at marker ", info.graph$names[x.n])
             if(info.graph$types[y.n]=="A.H.B")  mkty<-"AA : AB : BB (1:2:1) "
-        else if(info.graph$types[y.n]=="D.B")  mkty<-" Not BB : BB (3:1) "
-        else if(info.graph$types[y.n]=="C.A")  mkty<-" Not AA : AA (3:1) "
-        else if(info.graph$types[y.n]=="A.H")  mkty<-"AA : AB (1:1)"
-          else if(info.graph$types[y.n]=="M.X")  mkty<- "Mixed: Dominant & Co-dominant"
-        else stop ("invalid type of marker at marker ", info.graph$names[y.n])
-
+            else if(info.graph$types[y.n]=="D.B")  mkty<-" Not BB : BB (3:1) "
+            else if(info.graph$types[y.n]=="C.A")  mkty<-" Not AA : AA (3:1) "
+            else if(info.graph$types[y.n]=="A.H")  mkty<-"AA : AB (1:1)"
+            else if(info.graph$types[y.n]=="M.X")  mkty<- "Mixed: Dominant & Co-dominant"
+            else stop ("invalid type of marker at marker ", info.graph$names[y.n])
             LODScore<-info.graph$LOD[x.n, y.n]
+                
             ##information for message box (another classes)
             msg <- paste("Marker names: \n    ", info.graph$names[y.n], "\n    and \n    ", info.graph$names[x.n],
                          "\n\nMarker numbers:\n    ",mky.n," and ",mkx.n,
@@ -417,9 +422,9 @@ draw.rf.inter<-function(info.graph, scale, cex){
         else if (class(get(info.graph$data.name, pos=1))=="riself.onemap" || class(get(info.graph$data.name, pos=1))=="risib.onemap"){
             ##getting type of marker
             if(info.graph$types[x.n]=="A.B")  mktx<-"AA : BB (1:1)"
-          else stop ("invalid type of marker at marker ", info.graph$names[x.n])
+            else stop ("invalid type of marker at marker ", info.graph$names[x.n])
             if(info.graph$types[y.n]=="A.B")  mkty<-"AA : BB (1:1)"
-          else stop ("invalid type of marker at marker ", info.graph$names[y.n])  
+            else stop ("invalid type of marker at marker ", info.graph$names[y.n])  
             LODScore<-info.graph$LOD[x.n, y.n]
             ##information for message box (another classes)
             msg <- paste("Marker names: \n    ", info.graph$names[y.n], "\n    and \n    ", info.graph$names[x.n],
@@ -432,7 +437,8 @@ draw.rf.inter<-function(info.graph, scale, cex){
         else stop("invalid type of cross") 
         }
       else{
-          if((substr(info.graph$types[x.n],1,2)=="D1" && substr(info.graph$types[y.n],1,2)=="D2") || (substr(info.graph$types[x.n],1,2)=="D2" && substr(info.graph$types[y.n],1,2)=="D1")){
+          if((substr(info.graph$types[x.n],1,2)=="D1" && substr(info.graph$types[y.n],1,2)=="D2") ||
+             (substr(info.graph$types[x.n],1,2)=="D2" && substr(info.graph$types[y.n],1,2)=="D1")){
               msg <- paste("Marker names: \n    ", info.graph$names[x.n], "\n    and \n    ", info.graph$names[y.n],
                            "\n\nMarkers of type \n    ", info.graph$types[x.n], " and ", info.graph$types[y.n],
                            "\n\nImpossible to estimate recombination fraction via two-point.",
@@ -461,7 +467,6 @@ draw.rf.inter<-function(info.graph, scale, cex){
           else if(info.graph$types[x.n]=="M.X")  mktx<- "Mixed: Dominant & Co-dominant"
         else stop ("invalid type of marker at marker ", info.graph$names[x.n])
               
-
               if(info.graph$types[y.n]=="A.H.B")  mkty<-"AA : AB : BB (1:2:1) "
         else if(info.graph$types[y.n]=="D.B")  mkty<-" Not BB : BB (3:1) "
         else if(info.graph$types[y.n]=="C.A")  mkty<-" Not AA : AA (3:1) "
