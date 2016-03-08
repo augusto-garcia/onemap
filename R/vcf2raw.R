95afc0bf381ce8def2af239c11d1b3ad90ce83d4
gramarga <gramarga@gmail.com>
2016-03-08 16:47:37 -0300

    Including more details on vcf2raw documentation

--- R/vcf2raw.R
+++ R/vcf2raw.R
@@ -9,7 +9,7 @@
 # copyright (c) 2015, Gabriel R A Margarido                           #
 #                                                                     #
 # First version: 10/14/2015                                           #
-# Last update: 01/14/2016                                             #
+# Last update: 03/08/2016                                             #
 # License: GNU General Public License version 2 (June, 1991) or later #
 #                                                                     #
 #######################################################################
@@ -20,6 +20,10 @@
 ##' Converts data from a standard VCF (Variant Call Format) file to the input
 ##' format required by OneMap, while trying to identify the appropriate marker
 ##' segregation patterns.
+##'
+##' The input VCF file must be sorted, compressed and tabix indexed. Please check
+##' functions \code{bgzip} and \code{indexTabix} of package \code{Rsamtools} for
+##' details.
 ##'
 ##' Each variant in the VCF file is processed independently. Only biallelic SNPs
 ##' and indels for diploid variant sites are considered.
@@ -32,10 +36,20 @@
 ##' arguments.
 ##'
 ##' First, samples corresponding to both parents of the progeny are parsed and
-##' their genotypes identified, given that they are concordant above a provided
-##' threshold. Marker type is determined based on parental genotypes.
-##' Finally, progeny genotypes are identified and output is produced. Variants
-##' for which parent genotypes cannot be determined are discarded.
+##' their genotypes identified, given that their replicates are concordant above
+##' a threshold given by \code{min_class}. This allows replicates of the parents
+##' to be used, which is common in sequencing plates. In detail, each parent will
+##' be called an heterozygote only if
+##' \eqn{min\_class * number \ of \ replicates}{[min_class * number of replicates]}
+##' samples or more are heterozygous. The same is valid for homozygous calls.
+##' Whenever there are different genotypes among replicates, heterozygosity is
+##' checked first. The default value (\code{1.0}) requires that all replicates be
+##' of the same genotype. If each parent is represented by a single sample, this
+##' parameter has no effect.
+##'
+##' Next, marker type is determined based on parental genotypes. Finally, progeny
+##' genotypes are identified and output is produced. Variants for which parent
+##' genotypes cannot be determined are discarded.
 ##'
 ##' Reference sequence ID and position for each variant site are stored as special
 ##' fields denoted \code{CHROM} and \code{POS}.
@@ -59,12 +73,14 @@
 ##' @examples
 ##'
 ##'   \dontrun{
-##'     vcf2raw(input="your_VCF_file.vcf",
+##'     vcf2raw(input="your_VCF_file.vcf.gz",
 ##'             output="your_OneMap_file.raw",
 ##'             cross="your_cross_type",
 ##'             parent1=c("PAR1_sample1", "PAR1_sample2"),
 ##'             parent2=c("PAR2_sample1", "PAR2_sample2", "PAR2_sample3"),
-##'             min_class=0.5)
+##'             min_class=0.5) # for parent1, a single heterozygote replicate results
+##'                            # in a heterozygote genotype call; for parent2, at
+##'                            # least two samples have to be concordant
 ##'   }
 ##'
 
