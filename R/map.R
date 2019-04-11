009087a053e1bd6adc34d08077a78e555bdb8c5e
cristianetaniguti <chaytaniguti@gmail.com>
2019-04-11 14:36:32 -0300

    mds.seq argument in map and mds_onemap

--- R/map.R
+++ R/map.R
@@ -35,6 +35,9 @@
 ##' @param tol tolerance for the C routine, i.e., the value used to evaluate
 ##' convergence.
 ##' @param verbose If \code{TRUE}, print tracing information.
+##' @param mds.seq When some pair of markers do not follow the linkage criteria, 
+##' if \code{TRUE} one of the markers is removed and returns a vector with remaining 
+##' marker numbers (useful for mds_onemap function).
 ##' @return An object of class \code{sequence}, which is a list containing the
 ##' following components: \item{seq.num}{a \code{vector} containing the
 ##' (ordered) indices of markers in the sequence, according to the input file.}
@@ -81,111 +84,120 @@
 ##'   markers <- make_seq(twopt,c(30,12,3,14,2),phase=c(4,1,4,3)) # incorrect phases
 ##'   map(markers)
 ##'@export
-map <- function(input.seq,tol=10E-5, verbose=FALSE)
+map <- function(input.seq,tol=10E-5, verbose=FALSE, mds.seq=FALSE)
 {
-    ## checking for correct object
-    if(!("sequence" %in% class(input.seq)))
-        stop(deparse(substitute(input.seq))," is not an object of class 'sequence'")
-    ##Gathering sequence information
-    seq.num<-input.seq$seq.num
-    seq.phases<-input.seq$seq.phases
-    seq.rf<-input.seq$seq.rf
-    seq.like<-input.seq$seq.like
-    ##Checking for appropriate number of markers
-    if(length(seq.num) < 2) stop("The sequence must have at least 2 markers")
-    ##For F2, BC and rils
-    if(class(get(input.seq$data.name, pos=1))[2] == "f2")
-    {
-        final.map<-est_map_hmm_f2(geno=t(get(input.seq$data.name, pos=1)$geno[,seq.num]),
-                                  rf.vec=get_vec_rf_in(input.seq),
-                                  verbose=verbose,
-                                  tol=tol)
-        return(structure(list(seq.num=seq.num,
-                              seq.phases=seq.phases,
-                              seq.rf=final.map$rf,
-                              seq.like=final.map$loglike,
-                              data.name=input.seq$data.name,
-                              twopt=input.seq$twopt),
-                         class = "sequence"))
-    }
+  ## checking for correct object
+  if(!("sequence" %in% class(input.seq)))
+    stop(deparse(substitute(input.seq))," is not an object of class 'sequence'")
+  ##Gathering sequence information
+  seq.num<-input.seq$seq.num
+  seq.phases<-input.seq$seq.phases
+  seq.rf<-input.seq$seq.rf
+  seq.like<-input.seq$seq.like
+  ##Checking for appropriate number of markers
+  if(length(seq.num) < 2) stop("The sequence must have at least 2 markers")
+  ##For F2, BC and rils
+  if(class(get(input.seq$data.name, pos=1))[2] == "f2")
+  {
+    final.map<-est_map_hmm_f2(geno=t(get(input.seq$data.name, pos=1)$geno[,seq.num]),
+                              rf.vec=get_vec_rf_in(input.seq),
+                              verbose=verbose,
+                              tol=tol)
+    return(structure(list(seq.num=seq.num,
+                          seq.phases=seq.phases,
+                          seq.rf=final.map$rf,
+                          seq.like=final.map$loglike,
+                          data.name=input.seq$data.name,
+                          twopt=input.seq$twopt),
+                     class = "sequence"))
+  }
   else if(class(get(input.seq$data.name, pos=1))[2] == "backcross" ||
           class(get(input.seq$data.name, pos=1))[2] == "riself" ||
           class(get(input.seq$data.name, pos=1))[2] == "risib")
   {
-      final.map<-est_map_hmm_bc(geno=t(get(input.seq$data.name, pos=1)$geno[,seq.num]),
-                                rf.vec=get_vec_rf_in(input.seq),
-                                verbose=verbose,
-                                tol=tol)
-      if(class(get(input.seq$data.name, pos=1))[2] == "riself" ||
-         class(get(input.seq$data.name, pos=1))[2] == "risib")
-          final.map$rf<-adjust_rf_ril(final.map$rf,
-                                      type=class(get(input.seq$data.name, pos=1))[2],
-                                      expand = FALSE)
-      return(structure(list(seq.num=seq.num,
-                            seq.phases=seq.phases,
-                            seq.rf=final.map$rf,
-                            seq.like=final.map$loglike,
-                            data.name=input.seq$data.name,
-                            twopt=input.seq$twopt), class = "sequence"))
+    final.map<-est_map_hmm_bc(geno=t(get(input.seq$data.name, pos=1)$geno[,seq.num]),
+                              rf.vec=get_vec_rf_in(input.seq),
+                              verbose=verbose,
+                              tol=tol)
+    if(class(get(input.seq$data.name, pos=1))[2] == "riself" ||
+       class(get(input.seq$data.name, pos=1))[2] == "risib")
+      final.map$rf<-adjust_rf_ril(final.map$rf,
+                                  type=class(get(input.seq$data.name, pos=1))[2],
+                                  expand = FALSE)
+    return(structure(list(seq.num=seq.num,
+                          seq.phases=seq.phases,
+                          seq.rf=final.map$rf,
+                          seq.like=final.map$loglike,
+                          data.name=input.seq$data.name,
+                          twopt=input.seq$twopt), class = "sequence"))
   }
-
-    if((seq.phases == -1) && (seq.rf == -1) && is.null(seq.like)) {
-        ## if only the marker order is provided, without predefined linkage phases,
-        ## a search for the best combination of phases is performed and recombination
-        ## fractions are estimated
-        seq.phase <- numeric(length(seq.num)-1)
+  
+  if((seq.phases == -1) && (seq.rf == -1) && is.null(seq.like)) {
+    ## if only the marker order is provided, without predefined linkage phases,
+    ## a search for the best combination of phases is performed and recombination
+    ## fractions are estimated
+    seq.phase <- numeric(length(seq.num)-1)
+    results <- list(rep(NA,4),rep(-Inf,4))
+    
+    ## linkage map is started with the first two markers in the sequence
+    ## gather two-point information for this pair
+    phase.init <- vector("list",1)
+    list.init <- phases(make_seq(get(input.seq$twopt,pos = 1),seq.num[1:2],twopt=input.seq$twopt))
+    phase.init[[1]] <- list.init$phase.init[[1]]
+    Ph.Init <- comb_ger(phase.init)
+    for(j in 1:nrow(Ph.Init)) {
+      ## call to 'map' function with predefined linkage phase
+      temp <- map(make_seq(get(input.seq$twopt),seq.num[1:2],phase=Ph.Init[j],twopt=input.seq$twopt))
+      results[[1]][j] <- temp$seq.phases
+      results[[2]][j] <- temp$seq.like
+    }
+    seq.phase[1] <- results[[1]][which.max(results[[2]])] # best linkage phase is chosen
+    
+    if(length(seq.num) > 2) {
+      ## for sequences with three or more markers, these are added sequentially
+      for(mrk in 2:(length(seq.num)-1)) {
         results <- list(rep(NA,4),rep(-Inf,4))
-
-        ## linkage map is started with the first two markers in the sequence
-        ## gather two-point information for this pair
-        phase.init <- vector("list",1)
-	list.init <- phases(make_seq(get(input.seq$twopt,pos = 1),seq.num[1:2],twopt=input.seq$twopt))
-        phase.init[[1]] <- list.init$phase.init[[1]]
+        ## gather two-point information
+        phase.init <- vector("list",mrk)
+        list.init <- phases(make_seq(get(input.seq$twopt),c(seq.num[mrk],seq.num[mrk+1]),twopt=input.seq$twopt))
+        phase.init[[mrk]] <- list.init$phase.init[[1]]
+        for(j in 1:(mrk-1)) phase.init[[j]] <- seq.phase[j]
         Ph.Init <- comb_ger(phase.init)
         for(j in 1:nrow(Ph.Init)) {
-            ## call to 'map' function with predefined linkage phase
-            temp <- map(make_seq(get(input.seq$twopt),seq.num[1:2],phase=Ph.Init[j],twopt=input.seq$twopt))
-            results[[1]][j] <- temp$seq.phases
-            results[[2]][j] <- temp$seq.like
+          ## call to 'map' function with predefined linkage phases
+          temp <- map(make_seq(get(input.seq$twopt),seq.num[1:(mrk+1)],phase=Ph.Init[j,],twopt=input.seq$twopt))
+          results[[1]][j] <- temp$seq.phases[mrk]
+          results[[2]][j] <- temp$seq.like
         }
-        seq.phase[1] <- results[[1]][which.max(results[[2]])] # best linkage phase is chosen
-
-        if(length(seq.num) > 2) {
-            ## for sequences with three or more markers, these are added sequentially
-            for(mrk in 2:(length(seq.num)-1)) {
-                results <- list(rep(NA,4),rep(-Inf,4))
-                ## gather two-point information
-                phase.init <- vector("list",mrk)
-                list.init <- phases(make_seq(get(input.seq$twopt),c(seq.num[mrk],seq.num[mrk+1]),twopt=input.seq$twopt))
-                phase.init[[mrk]] <- list.init$phase.init[[1]]
-                for(j in 1:(mrk-1)) phase.init[[j]] <- seq.phase[j]
-                Ph.Init <- comb_ger(phase.init)
-                for(j in 1:nrow(Ph.Init)) {
-                    ## call to 'map' function with predefined linkage phases
-                    temp <- map(make_seq(get(input.seq$twopt),seq.num[1:(mrk+1)],phase=Ph.Init[j,],twopt=input.seq$twopt))
-                    results[[1]][j] <- temp$seq.phases[mrk]
-                    results[[2]][j] <- temp$seq.like
-                }
-                seq.phase[mrk] <- results[[1]][which.max(results[[2]])] # best combination of phases is chosen
-            }
+        if(all(is.na(results[[2]]))) {
+          if(mds.seq){
+            warning(cat("The linkage between markers", seq.num[mrk], "and", seq.num[mrk + 1], "did not reached the OneMap default criteria. They are probably segregating independently. Marker", seq.num[mrk+1], "will be removed.\n"))
+            return(seq.num[-(mrk+1)])
+            browser()
+          } else{
+            stop(paste("The linkage between markers", seq.num[mrk], "and", seq.num[mrk + 1], "did not reached the OneMap default criteria. They are probably segregating independently.\n"))
+          }
         }
-        ## one last call to map function, with the final map
-        map(make_seq(get(input.seq$twopt),seq.num,phase=seq.phase,twopt=input.seq$twopt))
+        seq.phase[mrk] <- results[[1]][which.max(results[[2]])] # best combination of phases is chosen
+      }
     }
+    ## one last call to map function, with the final map
+    map(make_seq(get(input.seq$twopt),seq.num,phase=seq.phase,twopt=input.seq$twopt))
+  }
   else {
-      ## if the linkage phases are provided but the recombination fractions have
-      ## not yet been estimated or need to be reestimated, this is done here
-      ## gather two-point information
-      rf.init <- get_vec_rf_out(input.seq, acum=FALSE)
-      ## estimate parameters
-      final.map <- est_map_hmm_out(geno=t(get(input.seq$data.name, pos=1)$geno[,seq.num]),
-                                   type=get(input.seq$data.name, pos=1)$segr.type.num[seq.num],
-                                   phase=seq.phases,
-                                   rf.vec=rf.init,
-                                   verbose=FALSE,
-                                   tol=tol)
-      return(structure(list(seq.num=seq.num, seq.phases=seq.phases, seq.rf=final.map$rf,
-                     seq.like=final.map$loglike, data.name=input.seq$data.name, twopt=input.seq$twopt), class = "sequence"))
+    ## if the linkage phases are provided but the recombination fractions have
+    ## not yet been estimated or need to be reestimated, this is done here
+    ## gather two-point information
+    rf.init <- get_vec_rf_out(input.seq, acum=FALSE)
+    ## estimate parameters
+    final.map <- est_map_hmm_out(geno=t(get(input.seq$data.name, pos=1)$geno[,seq.num]),
+                                 type=get(input.seq$data.name, pos=1)$segr.type.num[seq.num],
+                                 phase=seq.phases,
+                                 rf.vec=rf.init,
+                                 verbose=FALSE,
+                                 tol=tol)
+    return(structure(list(seq.num=seq.num, seq.phases=seq.phases, seq.rf=final.map$rf,
+                          seq.like=final.map$loglike, data.name=input.seq$data.name, twopt=input.seq$twopt), class = "sequence"))
   }
 }
 
