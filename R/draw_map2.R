924eb971140448513aa50bec72bbb751a8ffe4b7
Cristianetaniguti <chaytaniguti@gmail.com>
2020-02-07 12:36:57 -0300

    wip

--- R/draw_map2.R
+++ R/draw_map2.R
@@ -67,8 +67,8 @@ draw_map2<-function(...,tag=NULL,id=TRUE,pos =TRUE,cex.label=NULL,main=NULL,grou
   input<-list(...)
   if(length(input)==0) stop("argument '...' missing, with no default")
   map.data<-list()
-  for(i in seq(input)) map.data<-c(map.data,if(is(input[[i]], "list")) input[i] else do.call(c,input[i]))
-  if(!all(sapply(map.data, function(x) is(x, c("sequence","data.frame"))))) stop(paste("\n'",names(map.data)[!sapply(map.data, function(x) is(x, c("sequence","data.frame")))],"' is not an object of class 'sequence' or 'data.frame",sep=""))
+  for(i in seq(input)) map.data<-c(map.data,if(inherits(input[i], "list")) input[i] else do.call(c,input[i]))
+  if(!all(sapply(map.data, function(x) (is(x, "sequence") || is(x,"data.frame"))))) stop(paste("\n'",names(map.data)[!sapply(map.data, function(x)  (is(x, "sequence") || is(x,"data.frame")))],"' is not an object of class 'sequence' or 'data.frame",sep=""))
 
   #sequence to data.frame
   for(i in seq_along(map.data)){
