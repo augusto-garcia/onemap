7a44cca43d07eb73d0ea584cfb73c2f27289596b
cris_taniguti <chaytaniguti@gmail.com>
2020-03-02 10:51:37 -0300

    wip

--- R/pedsim2vcf.R
+++ R/pedsim2vcf.R
@@ -1,11 +1,9 @@
-## Bug in simulating F2
-
 #' Function to simulate depths and to convert PedigreeSim file to vcf file
 #' 
 #' Given PedigreeSim .dat .map and .chrom files generate vcf file with depths count
-#' in AD format field estimated by negative binomial. Works only for biallelic markers, 
-#' the allels can be named as A (reference) and B, or by DNA bases A, T, C and G. In this last case, 
-#' a vector indicanting the reference allele is needed.
+#' in AD format field estimated by negative binomial. The function receives genotypes codification in
+#' genotypes.dat according with Wu et. al 2002a table I observed bands. The a allele will be considered
+#' the reference allele. Null alleles are not supported.
 #' 
 #' @param inputfile file .dat output from PedigreeSim software
 #' @param map.file file .map input in PedigreeSim software
@@ -15,12 +13,14 @@
 #' @param disper.par dispersion parameter for negative binomial distribution
 #' @param mean.phred Sequencing error parameter
 #' @param chr.mb Chromossome size in mega base.
-#' @param method Choose negative binomial ("neg.binom") distribution or updog ("updog") model to simulate counts values
+#' @param method Choose negative binomial ("neg.binom"), poisson ("poisson") distributions or updog ("updog") model to simulate counts values
 #' @param miss.perc Percentage of missing data
 #' @param pos Phisical map position of each marker
 #' @param chr Chromosome where the marker is positioned
-#' @param haplo.ref The homologue or haplotype which correspond to the reference genome
 #' @param phase if TRUE the genotypes in VCF will be phased
+#' @param bias
+#' @param od
+#' @param disper.par
 #' 
 #' @return vcf file located in out.file defined path
 #' 
@@ -48,14 +48,13 @@ pedsim2vcf <- function(inputfile=NULL,
                        counts=TRUE, 
                        mean.depth=20, 
                        p.mean.depth = 20, 
-                       disper.par=2, 
                        chr.mb= 10, 
                        method = c("updog", "neg.binom", "poisson"), 
                        mean.phred=20, 
                        bias=0, 
                        od=0,
+                       disper.par=2,
                        pos=NULL,
-                       haplo.ref=NULL,
                        chr=NULL,
                        phase = FALSE){
   
@@ -69,144 +68,121 @@ pedsim2vcf <- function(inputfile=NULL,
   n.ind <- dim(data)[2]/2
   n.mk <- dim(data)[1]
   
-  # Reference haplotype - If there is no haploid reference, A will be the reference and B the alternative
-  if(is.null(haplo.ref)){
-    h.ref <- rep("a", n.mk)
-  } else{
-    h.ref <- data[,which(colnames(data) == haplo.ref)]
-    alt <- vector()
-    for(i in 1:n.mk)
-      alt <- c(alt, levels(factor(unlist(data[i,])))[which(levels(factor(unlist(data[i,]))) != h.ref[i])])
+  # Genotypes matrix 
+  idx <- rep(1:(n.ind), each = 2)
+  gt_matrix <- gt_ref <- gt_alt <-  het_matrix <- matrix(rep(NA,n.ind*n.mk), nrow = n.mk, ncol = n.ind)
+  
+  data <- as.matrix(data)
+  
+  if(any(data == "o"))
+    stop("Null alleles are not supported.")
+  
+  data[which(data == "a")] <- "0"
+  data[which(data == "b")] <- "1"
+  data[which(data == "c")] <- "2"
+  data[which(data == "d")] <- "3"
+  
+  for(i in 1:(length(idx)/2)){
+    gt_matrix[,i] <- paste0(data[,which(idx == i)[1]], "|", data[,which(idx == i)[2]])
+    het_matrix[,i] <- data[,which(idx == i)[1]] != data[,which(idx == i)[2]]
+    gt_ref[,i] <- data[,which(idx == i)[1]]
+    gt_alt[,i] <- data[,which(idx == i)[2]]
   }
   
-  # Genotypes matrix --- only for biallelic markers
-  idx <- rep(1:(n.ind), each = 2)
-  gt_matrix <- matrix(rep(NA,n.ind*n.mk), nrow = n.mk, ncol = n.ind)
-  if(phase){
-    for(j in 1:n.mk){
-      for(i in 1:(length(idx)/2)){
-        if(data[j,which(idx==i)[1]] == h.ref[j] & data[j,which(idx==i)[2]] == h.ref[j])
-          gt_matrix[j,i] <- "0|0"
-        if(data[j,which(idx==i)[1]] == h.ref[j] & data[j,which(idx==i)[2]] != h.ref[j])
-          gt_matrix[j,i] <- "0|1"
-        if(data[j,which(idx==i)[1]] != h.ref[j] & data[j,which(idx==i)[2]] == h.ref[j])
-          gt_matrix[j,i] <- "1|0"
-        if(data[j,which(idx==i)[1]] != h.ref[j] & data[j,which(idx==i)[2]] != h.ref[j])
-          gt_matrix[j,i] <- "1|1"
-      }
-    }
-  } else {
-    for(j in 1:n.mk){
-      for(i in 1:(length(idx)/2)){
-        if(data[j,which(idx==i)[1]] == h.ref[j] & data[j,which(idx==i)[2]] == h.ref[j])
-          gt_matrix[j,i] <- "0/0"
-        if(data[j,which(idx==i)[1]] == h.ref[j] & data[j,which(idx==i)[2]] != h.ref[j])
-          gt_matrix[j,i] <- "0/1"
-        if(data[j,which(idx==i)[1]] != h.ref[j] & data[j,which(idx==i)[2]] == h.ref[j])
-          gt_matrix[j,i] <- "0/1"
-        if(data[j,which(idx==i)[1]] != h.ref[j] & data[j,which(idx==i)[2]] != h.ref[j])
-          gt_matrix[j,i] <- "1/1"
-      }
-    }
+  if(!phase){
+    gt_matrix <- gsub("[|]", "/", gt_matrix)
+    gt_matrix[which(gt_matrix == "1/0")] <- "0/1"
+    gt_matrix[which(gt_matrix == "2/0")] <- "0/2"
+    gt_matrix[which(gt_matrix == "3/0")] <- "0/3"
+    gt_matrix[which(gt_matrix == "2/1")] <- "1/2"
+    gt_matrix[which(gt_matrix == "3/1")] <- "1/3"
+    gt_matrix[which(gt_matrix == "3/2")] <- "2/3"
   }
   
   if(counts==TRUE){
-    if(method=="neg.binom"){
-    # Negative binomial to estimate the depths (code adaptaded from Gusmap)
+    if(method=="neg.binom" | method=="poisson"){
+      # Negative binomial to estimate the depths (code adaptaded from Gusmap)
       depth <- prob.mat <- matrix(rep(NA, n.ind*n.mk),nrow=n.mk, ncol=n.ind)
       
-      prob.mat[gt_matrix == "0/0"] <- 1
-      prob.mat[gt_matrix == "0/1"] <- 0.5
-      prob.mat[gt_matrix == "1/1"] <- (10^(-mean.phred/10))
+      prob.mat[which(gt_matrix == "0/0" | gt_matrix == "0|0")] <- 1
+      prob.mat[het_matrix] <- 0.5
+      prob.mat[is.na(prob.mat)] <- (10^(-mean.phred/10))
       
-      depth[which(!is.na(gt_matrix))] <- rnbinom(sum(!is.na(gt_matrix)),mu=mean.depth,size=2)
-      
-      # Avoiding missing
-      idx <- which(depth==0)
-      
-      while(length(idx) >0){
-        depth[which(depth==0)] <- rnbinom(length(idx),mu=mean.depth,size=2)
+      if(method=="neg.binom"){
+        depth[which(!is.na(gt_matrix))] <- rnbinom(sum(!is.na(gt_matrix)),mu=mean.depth,size=2)
+        
+        # Avoiding missing
         idx <- which(depth==0)
-      }
-      
-      countsA <- matrix(rbinom(n.ind*n.mk, depth, prob.mat), nrow = n.mk)
-      countsB <- depth-countsA
-
-      # AD matrix
-      ad_matrix <- matrix(paste0(countsA,",",countsB), nrow = n.mk)
-      
-      info <- paste0("DP=", apply(depth,1,sum))
-      
-    } else if(method == "poisson"){
-      depth <- prob.mat <- matrix(rep(NA, n.ind*n.mk),nrow=n.mk, ncol=n.ind)
-      
-      prob.mat[gt_matrix == "0/0"] <- 1
-      prob.mat[gt_matrix == "0/1"] <- 0.5
-      prob.mat[gt_matrix == "1/1"] <- (10^(-mean.phred/10))
-      
-      depth[which(!is.na(gt_matrix))] <- rpois(sum(!is.na(gt_matrix)),mean.depth)
-      
-      # Avoiding missing
-      idx <- which(depth==0)
-      
-      while(length(idx) >0){
-        depth[which(depth==0)] <- rpois(length(idx),mean.depth)
-        idx <- which(depth==0)
-      }
-      
-      countsA <- matrix(rbinom(n.ind*n.mk, depth, prob.mat), nrow = n.mk)
-      countsB <- depth-countsA
-      
-      # AD matrix
-      ad_matrix <- matrix(paste0(countsA,",",countsB), nrow = n.mk)
-      
-      info <- paste0("DP=", apply(depth,1,sum))
-      
-      } else if(method=="updog"){
         
-        up_matrix <- size_matrix <- ref_matrix <- matrix(rep(NA, length(gt_matrix)), nrow = dim(gt_matrix)[1])
-        
-        idx <- which(gt_matrix == "0/1")
-        up_matrix[idx] <- 1
-        idx <- which(gt_matrix == "0/0")
-        up_matrix[idx] <- 0
-        idx <- which(gt_matrix == "1/1")
-        up_matrix[idx] <- 2
-        
-        size_matrix <- matrix(rnbinom(length(gt_matrix),mu=mean.depth,size=disper.par), nrow = dim(gt_matrix)[1])
-
-        # Parents with other depth
-        if(!is.null(p.mean.depth)){
-          size_matrix[,1:2] <- rnbinom(length(size_matrix[,1:2]),mu=p.mean.depth,size=disper.par)
+        while(length(idx) >0){
+          depth[which(depth==0)] <- rnbinom(length(idx),mu=mean.depth,size=2)
+          idx <- which(depth==0)
         }
+        ref_matrix <- matrix(rbinom(n.ind*n.mk, depth, prob.mat), nrow = n.mk)
         
+      } else {
+        depth[which(!is.na(gt_matrix))] <- rpois(sum(!is.na(gt_matrix)),mean.depth)
+        
+        # Avoiding missing
+        idx <- which(depth==0)
+        
+        while(length(idx) >0){
+          depth[which(depth==0)] <- rpois(length(idx),mean.depth)
+          idx <- which(depth==0)
+        }
+        ref_matrix <- matrix(rbinom(n.ind*n.mk, depth, prob.mat), nrow = n.mk)
+      }
+      
+      alt_matrix <- depth-ref_matrix      
+      
+      # AD matrix
+      ad_matrix <- matrix(paste0(ref_matrix,",",alt_matrix), nrow = n.mk)
+      
+      info <- paste0("DP=", apply(depth,1,sum))
+      
+    } else if(method=="updog"){
+      
+      up_matrix <- size_matrix <- ref_matrix <- matrix(rep(NA, length(gt_matrix)), nrow = dim(gt_matrix)[1])
+      
+      idx <- which(het_matrix)
+      up_matrix[idx] <- 1
+      idx <- which(gt_matrix == "0/0" | gt_matrix == "0|0")
+      up_matrix[idx] <- 0
+      idx <- which(is.na(up_matrix))
+      up_matrix[idx] <- 2
+      
+      size_matrix <- matrix(rnbinom(length(gt_matrix),mu=mean.depth,size=disper.par), nrow = dim(gt_matrix)[1])
+      
+      # Parents with other depth
+      if(!is.null(p.mean.depth)){
+        size_matrix[,1:2] <- rnbinom(length(size_matrix[,1:2]),mu=p.mean.depth,size=disper.par)
+      }
+      
+      mis <- which(size_matrix==0)
+      
+      while(length(mis) > 0){
+        size_matrix[mis] <- rnbinom(length(mis),mu=mean.depth,size=disper.par)
         mis <- which(size_matrix==0)
-        
-        while(length(mis) > 0){
-          size_matrix[mis] <- rnbinom(length(mis),mu=mean.depth,size=disper.par)
-          mis <- which(size_matrix==0)
-        }
-        
-        for(i in 1:dim(up_matrix)[1]){
-          ref_matrix[i,] <- updog::rflexdog(sizevec = size_matrix[i,],
-                          geno=up_matrix[i,],
-                          ploidy = 2,
-                          seq=10^(-mean.phred/10),
-                          bias=bias,
-                          od = od)
-        }
-        
-        ad_matrix <- matrix(paste0(size_matrix-ref_matrix, ",", ref_matrix), nrow = dim(up_matrix)[1])
-
-        info <- apply(size_matrix, 1, sum)
       }
+      
+      for(i in 1:dim(up_matrix)[1]){
+        ref_matrix[i,] <- updog::rflexdog(sizevec = size_matrix[i,],
+                                          geno=up_matrix[i,],
+                                          ploidy = 2,
+                                          seq=10^(-mean.phred/10),
+                                          bias=bias,
+                                          od = od)
+      }
+      
+      alt_matrix <- size_matrix-ref_matrix
+      
+      ad_matrix <- matrix(paste0(alt_matrix, ",", ref_matrix), nrow = dim(up_matrix)[1])
+      
+      info <- apply(size_matrix, 1, sum)
+    }
     
     # VCF format field
     format <- rep("GT:AD", n.mk)
-    
-    # Update geno matrix with counts
-    ref_matrix <- matrix(as.numeric(unlist(lapply(strsplit(ad_matrix, split = ","), "[[",1))), nrow = n.mk)
-    alt_matrix <- matrix(as.numeric(unlist(lapply(strsplit(ad_matrix, split = ","), "[[",2))), nrow = n.mk)
     
     # Select the number of less frequent allele
     minor_matrix <- matrix(rep(NA, n.mk*n.ind), nrow = n.mk, ncol = n.ind)
@@ -222,12 +198,28 @@ pedsim2vcf <- function(inputfile=NULL,
     
     # Reviewed matrix 
     check_matrix <- matrix(rep(NA, n.mk*n.ind), nrow = n.mk, ncol = n.ind)
-    check_matrix[which(het_matrix >= homo_matrix)] <- "0/1"
-    check_matrix[which(het_matrix == Inf)] <- "0/1"
-    idx <- which(het_matrix < homo_matrix) 
-    check_matrix[idx][ref_matrix[idx] > alt_matrix[idx]] <- "0/0"
-    check_matrix[idx][ref_matrix[idx] < alt_matrix[idx]] <- "1/1"
-    check_matrix[is.na(check_matrix)] <- "./."
+    
+    # search in initial file the alleles from heterozygotes
+    idx <- which(het_matrix >= homo_matrix | het_matrix == Inf)
+    check_matrix[idx][which(gt_ref[idx] != gt_alt[idx])] <- gt_matrix[idx][which(gt_ref[idx] != gt_alt[idx])]
+    
+    if(phase){ 
+      check_matrix[idx][which(gt_ref[idx] == gt_alt[idx])] <- paste0("0|",sapply(strsplit(gt_matrix[idx][which(gt_ref[idx] == gt_alt[idx])], "[|]"), unique))
+    } else { 
+      check_matrix[idx][which(gt_ref[idx] == gt_alt[idx])] <- paste0("0/",sapply(strsplit(gt_matrix[idx][which(gt_ref[idx] == gt_alt[idx])], "/"), unique))
+    }
+    
+    # search in initial file the alleles from homozigotes
+    idx <- which(het_matrix < homo_matrix)
+    check_matrix[idx][which(gt_ref[idx] == gt_alt[idx])] <- gt_matrix[idx][which(gt_ref[idx] == gt_alt[idx])]
+    
+    if(phase){ 
+      allele <- sapply(strsplit(gt_matrix[idx][which(gt_ref[idx] != gt_alt[idx])], "[|]"), "[", 1)
+      check_matrix[idx][which(gt_ref[idx] != gt_alt[idx])] <- paste0(allele, "|", allele) 
+    } else {
+      allele <- sapply(strsplit(gt_matrix[idx][which(gt_ref[idx] != gt_alt[idx])], "/"), "[", 1)
+      check_matrix[idx][which(gt_ref[idx] != gt_alt[idx])] <- paste0(allele, "/", allele) 
+    }
     
     chang <- table((gt_matrix == check_matrix))
     
@@ -271,61 +263,86 @@ pedsim2vcf <- function(inputfile=NULL,
     
     vcf_format <- matrix(paste0(check_matrix, ":", ad_matrix), nrow = n.mk)
     
-    } else {
-        vcf_format <- gt_matrix
-        format <- rep("GT", n.mk)
-        info <- "."
-    }
-    
-   # Adding missing data
-    miss <- sample(1:length(vcf_format), length(vcf_format)*(miss.perc/100))
-    if(length(miss)>0){
-      if(counts){
-        vcf_format[miss] <- "./.:0,0"  
-      } else{ vcf_format[miss] <- "./." }
-    }
-      
-      
-    names1 <- lapply(strsplit(colnames(data)[-1], "_"), function (x) x[-length(x)])
-    names1 <- unique(sapply(names1, function(x) if(length(x)>1) paste0(x[1], "_", x[2]) else x)) 
-    
-    colnames(vcf_format) <- names1
+  } else {
+    vcf_format <- gt_matrix
+    format <- rep("GT", n.mk)
+    info <- "."
+  }
   
-    if(is.null(chr)){
+  # Adding missing data
+  miss <- sample(1:length(vcf_format), length(vcf_format)*(miss.perc/100))
+  if(length(miss)>0){
+    if(counts){
+      vcf_format[miss] <- "./.:0,0"  
+    } else{ vcf_format[miss] <- "./." }
+  }
+  
+  names1 <- lapply(strsplit(colnames(data)[-1], "_"), function (x) x[-length(x)])
+  names1 <- unique(sapply(names1, function(x) if(length(x)>1) paste0(x[1], "_", x[2]) else x)) 
+  
+  colnames(vcf_format) <- names1
+  
+  if(is.null(chr)){
     chr.info <- read.table(map.file, header = TRUE, stringsAsFactors = FALSE)
     chr <- chr.info$chromosome
-    }
-    
-    if(is.null(pos)){
+  }
+  
+  if(is.null(pos)){
     pos.info <- read.table(chrom.file, header = TRUE, stringsAsFactors = FALSE)
     pos <- chr.info$position*((chr.mb*1000)/mean(pos.info$length))
-    } 
-    
-    id <- rownames(data)
-    if(is.null(haplo.ref)){
-      ref <- sample(c("A","T", "C", "G"), n.mk, replace = TRUE)
-      alt <- sample(c("A","T", "C", "G"), n.mk, replace = TRUE)
-      
-      while(!all(!ref==alt)){
-        alt[which(ref==alt)] <- sample(c("A","T", "C", "G"), length(which(ref==alt)), replace = TRUE)
-      }
-    } else{
-      ref <- h.ref 
-      alt <- alt
+  } 
+  
+  id <- rownames(data)
+  
+  # Defininf alleles in field REF and ALT
+  ## REF
+  ref <- sample(c("A","T", "C", "G"), n.mk, replace = TRUE)
+  alt <- rep(NA, length(ref))
+  ## ALT
+  done <- vector() # vector to store markers already avaliated
+  for(j in c(3,2,1)){
+    if(length(done) != 0){
+      alleles <- apply(check_matrix[-done,], 1, function(x) any(grepl(j, x)))
+    } else {
+      alleles <- apply(check_matrix, 1, function(x) any(grepl(j, x)))
     }
-    
-    qual <- filter <- rep(".", n.mk)
-    # transformar em vetor
-    vcf_file_mks <- data.frame("CHROM"=chr, "POS"=pos, "ID"= id,"REF"=ref, "ALT"=alt, 
-                               "QUAL"=qual, "FILTER"=filter,"INFO"=info,"FORMAT"=format,vcf_format, stringsAsFactors = FALSE)
+    if(sum(alleles) != 0){
+      alt_temp <- matrix(NA, nrow=sum(alleles), ncol = 3)
+      ref_temp <- ref[alleles]
+      
+      for(i in 1:sum(alleles)){
+        temp <- sample(c("A","T", "C", "G"), 4, replace = F)
+        alt_temp[i,] <- temp[-which(temp == ref_temp[i])]
+      }
+      rm.colum <- 3-j
+      if(rm.colum != 0){
+        alt_temp <- alt_temp[,-c(1:rm.colum)]
+      }
+      if(length(done) != 0){
+        if(is(alt_temp, "matrix"))
+          alt[-done][alleles] <- apply(alt_temp, 1, function(x) paste0(x, collapse = ","))
+        else alt[-done][alleles] <- alt_temp
+      } else {
+        if(is(alt_temp, "matrix"))
+          alt[alleles] <- apply(alt_temp, 1, function(x) paste0(x, collapse = ","))
+        else alt[alleles] <- alt_temp
+      }
+      done <- c(done,which(alleles))
+    }
+  }
   
-    vcf_vector <- apply(vcf_file_mks, 1, function(x) paste(x, collapse = "\t"))
+  qual <- filter <- rep(".", n.mk)
+  # transformar em vetor
+  vcf_file_mks <- data.frame("CHROM"=chr, "POS"=pos, "ID"= id,"REF"=ref, "ALT"=alt, 
+                             "QUAL"=qual, "FILTER"=filter,"INFO"=info,"FORMAT"=format,vcf_format, stringsAsFactors = FALSE)
   
-    header1 <- paste0(colnames(vcf_file_mks), collapse = "\t")
-    header <- paste0("##fileformat=VCFv4.1", "\n", "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">", '\n',
+  vcf_vector <- apply(vcf_file_mks, 1, function(x) paste(x, collapse = "\t"))
+  
+  header1 <- paste0(colnames(vcf_file_mks), collapse = "\t")
+  header <- paste0("##fileformat=VCFv4.1", "\n", "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">", '\n',
                    "##FORMAT=<ID=AD,Number=.,Type=Integer,Description=\"Allelic depths for the reference and alternate alleles in the order listed\">",'\n',
                    "##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">", "\n", "#", header1)
   
-    vcf <- c(header,vcf_vector)
-    write.table(vcf, file = paste(out.file), quote = FALSE, row.names = FALSE,  col.names = FALSE)
+  vcf <- c(header,vcf_vector)
+  write.table(vcf, file = paste(out.file), quote = FALSE, row.names = FALSE,  col.names = FALSE)
 }
