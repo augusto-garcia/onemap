c7ebf0f796d0a4401d665b4033a969feaa30a1f9
cristianetaniguti <chaytaniguti@gmail.com>
2022-02-09 12:41:08 -0600

    bugifx

--- R/combine_onemap.R
+++ R/combine_onemap.R
@@ -231,13 +231,13 @@ split_onemap <- function(onemap.obj=NULL, mks=NULL){
     
     if(!inherits(onemap.obj, c("onemap"))) stop("Input object must be of class onemap")
 
-    if(inherits(mks, "character")){
+    if(is(mks, "character")){
         idx.mks <- which(colnames(onemap.obj$geno) %in% mks)
         rev.mks <- which(!colnames(onemap.obj$geno) %in% mks)
         if(any(!mks %in% colnames(onemap.obj$geno))) 
             stop("One or more of the selected markers do not exist in the onemap object")
             
-    } else if(inherits(mks, c("numeric"))){
+    } else if(is(mks, "numeric")){
         idx.mks <- mks
         if(any(mks > onemap.obj$n.mar)) 
             stop("One or more of the selected markers do not exist in the onemap object")
